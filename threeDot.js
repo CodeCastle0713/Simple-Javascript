4ae7583dc079b4ef3e7ac508b1f7ffc7982017e2
CodeCastle0713 <true.dragon.p@gmail.com>
2024-09-12 21:18:04 +0900

    review the code again.

--- threeDot.js
+++ threeDot.js
@@ -2,5 +2,8 @@ let fruits = ["Mango", "Apple", "Mango", "Banana", "Mango"];
 
 // Output the unique array elements in fruits.
 const realFruits = [...new Set(fruits)];
+const newSet = new Set(fruits); //Save the unique elements after using <Set>
+const myBool = newSet.has("Mango");
 
 console.log(realFruits);
+console.log(myBool);
